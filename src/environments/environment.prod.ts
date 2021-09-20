f72d1b68f3850831806d37d306034be875ea3a17
Vladimir Yakovlev <nagos@inbox.ru>
2021-09-20 17:29:10 +0300

    Адрес eventlog в environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 const loc = window.location;
 export const environment = {
     dev_ip: 'ws://' + loc.host + '/app',
+    log_url: 'http://'+loc.host+'/log',
     production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ import { DEV_IP } from '../../debug';
 
 export const environment = {
     dev_ip: 'ws://' + DEV_IP + '/app',
+    log_url: 'http://'+DEV_IP+'/log',
     production: false
 };
