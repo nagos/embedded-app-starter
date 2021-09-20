6372d77b0d8085f6b03afba90fcf324b674271be
Vladimir Yakovlev <nagos@inbox.ru>
2021-09-20 14:35:14 +0300

    Опция nolabel в fieldbody

--- src/app/components/fieldbody/fieldbody.component.ts
+++ src/app/components/fieldbody/fieldbody.component.ts
@@ -10,11 +10,17 @@ export class FieldBodyComponent {
     @Input() title = '';
     public labelClass = 'col-xl-5 col-lg-4 col-md-12';
     public inputClass = 'col-xl-7 col-lg-8 col-md-12';
+    public inputClassNoLabel = 'col-xl-12 col-lg-12 col-md-12 text-center';
     @Input() set nocase(value: boolean) {
         // защита от пустой строки
         this.isNocase = typeof value === 'boolean' ? value : true;
     }
+    @Input() set nolabel(value: boolean) {
+        // защита от пустой строки
+        this.isNoLabel = typeof value === 'boolean' ? value : true;
+    }
     public isNocase = false;
+    public isNoLabel = false;
 
     constructor() {
     }
