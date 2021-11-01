65470a50eeff449dff06825362222dd6209e1d9e
Vladimir Yakovlev <nagos@inbox.ru>
2021-11-01 10:25:54 +0300

    Обновлен эмулятор сервера

--- server_emulator.js
+++ server_emulator.js
@@ -1,36 +1,52 @@
-// Node.js WebSocket server script
-const rand_value = Math.floor(Math.random()*1000);
-const http = require('http');
-const WebSocketServer = require('websocket').server;
-const server = http.createServer();
-server.listen(9898);
-const wsServer = new WebSocketServer({
-    httpServer: server
+const WebSocket = require('ws')
+const fs = require('fs');
+const path = require('path');
+let watchFiles = [];
+
+function send_file(fname) {
+    fs.readFile(fname, 'utf8' , (err, data) => {
+        const msgType = path.basename(fname, ".json");
+        const msgData = JSON.parse(data);
+        wsServer.clients.forEach(function(client) {
+            client.send(JSON.stringify({
+                type: msgType,
+                data: msgData,
+            }));
+         });
+    });
+}
+
+function watchFile(fname) {
+    fs.watchFile(fname, {interval: 1000}, () => {
+        send_file(fname);
+    });
+}
+
+fs.readdirSync('./server_emulator/').forEach(file => {
+    watchFiles.push('./server_emulator/' + file);
 });
-wsServer.on('request', function (request) {
-    const connection = request.accept(null, request.origin);
-    connection.on('message', function (message) {
-        const msg = JSON.parse(message.utf8Data);
-        console.log('Received Message:', msg.type);
+
+const wsServer = new WebSocket.Server({ port: 9898 },()=>{
+    console.log('server started')
+});
+
+wsServer.on('connection', (ws) => {
+    ws.on('message', function (message) {
+        const msg = JSON.parse(message);
         if(msg.type=='keepalive') {
-            connection.send(JSON.stringify({
+            ws.send(JSON.stringify({
                 type: "keepalive", data: {}
             }));
         } else {
             console.log(msg);
         }
     });
-    connection.on('close', function (reasonCode, description) {
-        console.log('Client has disconnected.');
-    });
-    connection.send(JSON.stringify({
-        type: "settings", data: {
-            value: 'New Value',
-            valueint: 1,
-            valueselect: 1,
-            valuebutton: 0,
-            timeZone: 'Europe/Moscow',
-            rand_value: rand_value,
-        }
-    }));
+    watchFiles.forEach((fname) => {
+        send_file(fname);
+    })
+
+});
+
+watchFiles.forEach((fname) => {
+    watchFile(fname);
 });
